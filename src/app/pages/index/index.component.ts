e8975bc7ffbbd7ca1cc837e2c50a3406c4eee405
Lupita <guadalupe.hdz.contre@gmail.com>
2026-02-27 13:32:53 -0600

    commit

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index',
-  imports: [NavbarComponent],
+  standalone: true,
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.css'
+  styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
+
+  alumnos = [
+    {
+      control: '2023001',
+      nombre: 'Alejandro',
+      carrera: 'Sistemas',
+      semestre: 6,
+      especialidad: 'Redes'
+    },
+    {
+      control: '2023002',
+      nombre: 'María',
+      carrera: 'Administración',
+      semestre: 4,
+      especialidad: 'Finanzas'
+    }
+  ];
+
+  eliminar(index: number) {
+    this.alumnos.splice(index, 1);
+  }
+
+  editar(index: number) {
+    alert('Aquí luego conectamos el modal de edición 😎');
+  }
 
 }
